8e38fc1ab9bf1931acee533e58efb7b42a5e905c
agostinoluca <agostino.luca89@gmail.com>
2024-01-21 18:30:52 +0100

    add h1 message

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      message: 'Ciao, benvenuto in questo nuovo esercizio!',
+      image: 'https://picsum.photos/200/300'
+    }
+  }
+}
 </script>
 
 <template>
+  <header>
+    <h1>{{ message }}</h1>
+  </header>
+  
+  <main>
+
+  </main>
+
+  <footer>
+
+  </footer>
 </template>
 
 <style>
